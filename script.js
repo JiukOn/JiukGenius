23073b872143ea5558809d409240b5573e63ff28
Glitch (glitch-hello-website) <none>
2021-11-28 18:50:22 +0000

    🎹🦀 Checkpoint ./style.css:25114002/588 ./script.js:25114002/1262

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+let order = [];
+let clickedOrder = [];
+let score = 0;
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+// 0-pink  1-red  2-purple  3-blue
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
-}
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const purple = document.querySelector('.purple');
+const pink = document.querySelector('.pink');
